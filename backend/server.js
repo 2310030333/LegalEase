518ca3bdcea2d7fba4f45098591dc05d87d67957
Sri Aashritha <sriaashrikarthi@gmail.com>
2025-04-28 09:29:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(express.json({ limit: '50mb' })); // Increase payload size limit
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/lawyer', lawyersearch);
+app.use('/api/notifications', notificationRoutes);
+app.use('/api/appointments', appointmentRoutes);
+
 
 mongoose.connect("mongodb+srv://2310030333:chinni12@cluster0.tzyfxzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
